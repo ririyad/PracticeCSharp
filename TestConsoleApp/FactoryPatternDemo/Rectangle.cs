842e0b7da2778181287181babc825d5ae8660621
Rashedul Islam <ririyad.291@gmail.com>
2016-09-18 22:17:00 +0600

    added Factory Pattern in CSharp

---
+++ TestConsoleApp/FactoryPatternDemo/Rectangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactoryPatternDemo
+{
+    class Rectangle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Inside from Rectangle class");
+        }
+    }
+}
---
+++ TestConsoleApp/FactoryPatternDemo/ShapeFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternDemo
+{
+    class ShapeFactory
+    {
+        public IShape GetShape(string shapeType)
+        {
+            if (shapeType == null)
+                return null;
+            else if (shapeType.Equals("circle", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return new Circle();
+            }
+
+            else if (shapeType.Equals("triangle", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return new Triangle();
+            }
+
+            else if (shapeType.Equals("rectangle", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return new Rectangle();
+            }
+
+            return null;
+        }
+    }
+}
