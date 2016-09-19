5b33bbba25ed7264271d333156560302f64c3dcd
Rashedul Islam <ririyad.291@gmail.com>
2016-09-19 07:24:57 +0600

    added practice codes

---
+++ TestConsoleApp/TestArrayList/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add(2);
+            arrayList.Add(3);
+            for (int i = 4; i <= 10; i++)
+            {
+                arrayList.Add(i);
+            }
+            arrayList.Add(43);
+            arrayList.Add(23);
+            arrayList.Add(67);
+            arrayList.Add(22);
+            arrayList.Add(98);
+            arrayList.Add(32);
+
+            Console.WriteLine("Capacity: {0}", arrayList.Capacity);
+            Console.WriteLine("Count: {0}", arrayList.Count);
+
+            foreach (int i in arrayList)
+            {
+                Console.WriteLine(i + " ");
+            }
+
+            Console.WriteLine();
+            Console.Write("Sorted Content: ");
+            arrayList.Sort();
+            foreach (int i in arrayList)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            arrayList.Clear();
+            Console.WriteLine(arrayList.Count);
+            Console.WriteLine(arrayList.Capacity);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestConsoleApp/TestHashTable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hashtable hashtable = new Hashtable();
+            SortedList hashtable = new SortedList();
+            hashtable.Add("01", "Zara");
+            hashtable.Add("02", "Abida");
+            hashtable.Add("03", "Masum");
+            hashtable.Add("04", "Arif");
+
+            if (hashtable.ContainsValue("Nuha"))
+            {
+                Console.WriteLine("The student is already in the list");
+            }
+            else
+            {
+                hashtable.Add("05", "Nuha");
+            }
+
+            // get a collection of keys
+            ICollection key = hashtable.Keys;
+            foreach (string s in key)
+            {
+                Console.WriteLine(s + " " + hashtable[s]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
