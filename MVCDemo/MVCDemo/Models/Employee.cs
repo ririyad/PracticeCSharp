9765016889c6337272ad2f04529e8bb066ccdabe
riyad291 <ririyad.291@gmail.com>
2016-09-24 10:48:51 +0600

    added MVCDemo project

---
+++ MVCDemo/MVCDemo/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    [Table("tbl_Employee")]
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ TezzCricket/TezzCricket/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TezzCricket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TotalRun totalRun = new TotalRun();
+            int testCase = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= testCase; i++)
+            {
+                int[] arr;
+                int noOfOut = Convert.ToInt32(Console.ReadLine());
+                var run = totalRun.AddRuns();
+
+                Console.WriteLine("Case: {0}: {1}", i, run);
+            }
+        }
+    }
+
+    class TotalRun
+    {
+        public int AddRuns(params int[] arr)
+        {
+            int sum = 0;
+            foreach (int i in arr)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+    }
+}
